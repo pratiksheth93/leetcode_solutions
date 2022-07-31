12b71bcdde5268ea65a35483e21e6933d88175a6
Pratik Sheth <pratik_sheth@rocketmail.com>
2022-07-30 23:16:20 -0700

    Add Coin Change problem

---
+++ Array/Coin Change/Solution.py
@@ -0,0 +1,13 @@
+# Time Complexity: O(S * N)
+# Space Complexity: O(S)
+# S: Amount
+
+class DynamicProgramming:
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        dp = [float('inf')] * (amount + 1)
+        dp[0] = 0
+        
+        for coin in coins:
+            for x in range(coin, amount + 1):
+                dp[x] = min(dp[x], dp[x - coin] + 1)
+        return dp[amount] if dp[amount] != float('inf') else -1 
