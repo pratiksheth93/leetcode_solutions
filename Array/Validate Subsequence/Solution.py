61fe8406aedee0f24b1cbf9e39891c58ad1b555e
Pratik Sheth <pratik_sheth@rocketmail.com>
2022-07-31 20:06:49 -0700

    Added Validate Subarray

---
+++ Array/Validate Subsequence/Solution.py
@@ -0,0 +1,12 @@
+# Time Complexity: O(N)
+# Space Complexity: O(1)
+
+class Solution:
+    def isValidSubsequence(array:List[int], sequence:List[int]):
+        index = 0
+	    for value in array:
+		    if index == len(sequence):
+			    break
+		    if sequence[index] == value:
+			    index+=1
+	    return index == len(sequence)
