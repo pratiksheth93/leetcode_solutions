09e797f6621adc13834985a44ea85593e663cd0b
Pratik Sheth <pratik_sheth@rocketmail.com>
2022-07-31 23:03:08 -0700

    Add Three Sum Closet

---
+++ Array/Three Sum Closet/Solution.py
@@ -0,0 +1,40 @@
+# Time Complexity: O(N^2)
+# Space Complexity: O(N* Log N)
+
+class TwoPointer:
+    def threeSumClosest(self, nums: List[int], target: int) -> int:
+        diff = float('inf')
+        nums.sort()
+        for i in range(len(nums)):
+            lo, hi = i + 1, len(nums) - 1
+            while (lo < hi):
+                sum = nums[i] + nums[lo] + nums[hi]
+                if abs(target - sum) < abs(diff):
+                    diff = target - sum
+                if sum < target:
+                    lo += 1
+                else:
+                    hi -= 1
+            if diff == 0:
+                break
+        return target - diff
+
+# Time Complexity: O(N^2 * Log N)
+# Space Complexity: O(N)
+
+class BinarySearch:
+    def threeSumClosest(self, nums: List[int], target: int) -> int:
+        diff = float('inf')
+        nums.sort()
+        for i in range(len(nums)):
+            for j in range(i + 1, len(nums)):
+                complement = target - nums[i] - nums[j]
+                hi = bisect_right(nums, complement, j + 1)
+                lo = hi - 1
+                if hi < len(nums) and abs(complement - nums[hi]) < abs(diff):
+                    diff = complement - nums[hi]
+                if lo > j and abs(complement - nums[lo]) < abs(diff):
+                    diff = complement - nums[lo]
+            if diff == 0:
+                break
+        return target - diff
